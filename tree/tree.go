0418ebd33a2a432bf5a9ed8a2754e60568154b22
chinmaysomani <chinmaysomani7@gmail.com>
2022-11-16 13:31:21 +0530

    changes for resuing the code

--- tree/tree.go
+++ tree/tree.go
@@ -4,222 +4,127 @@ import (
 	"fmt"
 	"io/fs"
 	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
 	"sort"
-	"strings"
 )
 
-type Command struct {
-	command  string
-	filepath string
-}
+// create struct of option and value and desc
+
+// tree -p -L 3 /Users/chinmaysomani/Desktop/gocodes
+// create a slice of this struct
+
+// tree -p -d /Users/chinmaysomani/Desktop/gocodes
 
 func main() {
 
-	inputCommand := enterCommand()
-	//printFilePath(inputCommand.command, inputCommand.filepath)
-
-	// inputCommand = enterCommand()
-	// searchDirectoryOnly(inputCommand.command, inputCommand.filepath)
-
-	//printRelativeFilePath(inputCommand.command, inputCommand.filepath)
-
-	//printFilePathWithPermission(inputCommand.command, inputCommand.filepath)
-
-	sortWithModificationTime(inputCommand.command, inputCommand.filepath)
+	info, paths := recursive(os.Args[2])
+	if os.Args[1] == "-t" {
+		acctomodtime(info, paths)
+	} else if os.Args[1] == "-f" {
+		printRelativePath(info, paths)
+	} else if os.Args[1] == "-p" {
+		printWithPermission(info)
+	} else if os.Args[1] == "-d" {
+		printDirectoryOnly(info)
+	} else {
+		printPathOfFiles(info)
+	}
 }
 
-func enterCommand() Command {
+func recursive(file string) ([]os.FileInfo, []string) {
 
-	fmt.Println("Enter the command:")
-	var command string
-	var filepath string
-	fmt.Scan(&command, &filepath)
-
-	data := Command{command, filepath}
-	return data
-}
-
-func printFilePath(command string, file string) {
-
-	noOfDirectories := 0
-	noOfFiles := 0
-
-	checkValidCommand(command)
-
+	mydirectoryslice := make([]os.FileInfo, 0)
+	pathSlice := make([]string, 0)
 	err := filepath.Walk(file,
 		func(path string, info os.FileInfo, err error) error {
+
+			pathSlice = append(pathSlice, path)
 
 			if err != nil {
 				return err
 			}
 
-			if info.IsDir() {
-				fmt.Println("|   ")
-				fmt.Printf("|-- %s\n", info.Name())
-				noOfDirectories++
-
-			} else {
-				fmt.Println("|   ")
-				fmt.Printf("|------- %s\n", info.Name())
-				noOfFiles++
-			}
-
+			mydirectoryslice = append(mydirectoryslice, info)
 			return nil
 		})
 
-	fmt.Printf("%v directories, %v files\n", noOfDirectories, noOfFiles)
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
+	}
+	return mydirectoryslice, pathSlice
+}
+
+func printPathOfFiles(structure []os.FileInfo) {
+	for i := 0; i < len(structure); i++ {
+		if structure[i].IsDir() {
+			fmt.Println("|   ")
+			fmt.Printf("|-- %s\n", structure[i].Name())
+		} else {
+			fmt.Println("|   ")
+			fmt.Printf("|------- %s\n", structure[i].Name())
+		}
 	}
 }
 
-func printRelativeFilePath(command string, file string) {
-	noOfDirectories := 0
-	noOfFiles := 0
+func printDirectoryOnly(structure []os.FileInfo) {
+	for i := 0; i < len(structure); i++ {
+		if structure[i].IsDir() {
+			fmt.Println("|   ")
+			fmt.Printf("|-- %s\n", structure[i].Name())
+		}
+	}
+}
 
-	checkValidCommand(command)
+func printRelativePath(myslice []os.FileInfo, pathSlice []string) {
 
-	err := filepath.Walk(file,
-		func(path string, info os.FileInfo, err error) error {
+	for i := 0; i < len(myslice); i++ {
+		if myslice[i].IsDir() {
+			fmt.Println("|   ")
+			fmt.Printf("|-- %s\n", pathSlice[i])
+		} else {
+			fmt.Println("|   ")
+			fmt.Printf("|------- %s\n", pathSlice[i])
+		}
+	}
+}
 
+func printWithPermission(infoslice []os.FileInfo) {
+	for i := 0; i < len(infoslice); i++ {
+		if infoslice[i].IsDir() {
+			fmt.Println("|   ")
+			fmt.Printf("|--[%v] %v\n", infoslice[i].Mode(), infoslice[i].Name())
+		} else {
+			fmt.Println("|   ")
+			fmt.Printf("|------- [%v]%v\n", infoslice[i].Mode(), infoslice[i].Name())
+		}
+	}
+}
+
+func acctomodtime(infoslice []os.FileInfo, paths []string) {
+
+	var files []fs.FileInfo
+	var err error
+
+	for i := 0; i < len(infoslice); i++ {
+		if infoslice[i].IsDir() {
+			fmt.Println("|   ")
+			fmt.Printf("|-- %s\n", infoslice[i].Name())
+			p := paths[i]
+			files, err = ioutil.ReadDir(p)
 			if err != nil {
-				return err
+				fmt.Println(err)
+			}
+		} else {
+			sort.Slice(files, func(i, j int) bool {
+				return files[i].ModTime().Before(files[j].ModTime())
+			})
+
+			for i := 0; i < len(files); i++ {
+				fmt.Printf("|------- %s %v\n", files[i].Name(), files[i].ModTime())
 			}
 
-			if info.IsDir() {
-				fmt.Println("|   ")
-				fmt.Printf("|-- %s\n", path)
-				noOfDirectories++
-
-			} else {
-				fmt.Println("|   ")
-				fmt.Printf("|------- %s\n", path)
-				noOfFiles++
-			}
-
-			return nil
-		})
-
-	fmt.Printf("%v directories, %v files\n", noOfDirectories, noOfFiles)
-	if err != nil {
-		log.Println(err)
+			files = nil
+		}
 	}
-}
-
-func printDirectoryOnly(command string, file string) {
-	noOfDirectories := 0
-
-	checkValidCommand(command)
-
-	err := filepath.Walk(file,
-		func(path string, info os.FileInfo, err error) error {
-
-			if info.IsDir() {
-				fmt.Println("|   ")
-				fmt.Printf("|-- %s\n", info.Name())
-				noOfDirectories++
-			}
-			return nil
-		})
-
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	fmt.Println(noOfDirectories, " directories")
-}
-
-func printFilePathWithPermission(command string, file string) {
-
-	noOfDirectories := 0
-	noOfFiles := 0
-
-	checkValidCommand(command)
-
-	err := filepath.Walk(file,
-		func(path string, info os.FileInfo, err error) error {
-
-			info.ModTime()
-
-			if err != nil {
-				return err
-			}
-
-			if info.IsDir() {
-				fmt.Println("|   ")
-				fmt.Printf("|--[%v] %v\n", info.Mode(), info.Name())
-				noOfDirectories++
-
-			} else {
-				fmt.Println("|   ")
-				fmt.Printf("|------- [%v]%v\n", info.Mode(), info.Name())
-				noOfFiles++
-			}
-
-			return nil
-		})
-
-	fmt.Printf("%v directories, %v files\n", noOfDirectories, noOfFiles)
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func sortWithModificationTime(command string, file string) {
-	noOfDirectories := 0
-	noOfFiles := 0
-	files := make([]fs.FileInfo, 0)
-
-	checkValidCommand(command)
-
-	err := filepath.Walk(file,
-		func(path string, info os.FileInfo, err error) error {
-
-			if err != nil {
-				return err
-			}
-
-			if info.IsDir() {
-				fmt.Println("|   ")
-				fmt.Printf("|-- %s\n", info.Name())
-				files, err = ioutil.ReadDir(path)
-				if err != nil {
-					fmt.Println(err.Error())
-				}
-				noOfDirectories++
-
-			} else {
-
-				sort.Slice(files, func(i, j int) bool {
-					return files[i].ModTime().Before(files[j].ModTime())
-				})
-
-				for i := 0; i < len(files); i++ {
-					fmt.Printf("|------- %s %v\n", files[i].Name(), files[i].ModTime())
-				}
-
-				noOfFiles++
-				files = nil
-			}
-
-			return nil
-		})
-
-	fmt.Printf("%v directories, %v files\n", noOfDirectories, noOfFiles)
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func checkValidCommand(command string) bool {
-
-	validCommand := true
-	if !strings.EqualFold(command, "TREE") {
-		validCommand = false
-		log.Fatal("WRONG COMMAND ENTERED. PLEASE ENTER tree <filepath>")
-	}
-	return validCommand
 }
