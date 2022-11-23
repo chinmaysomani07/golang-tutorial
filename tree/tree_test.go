5d3e44bdf42359053dd6e1ff6d3e327178491537
chinmaysomani <chinmaysomani7@gmail.com>
2022-11-23 09:54:44 +0530

    modify test file

--- tree/tree_test.go
+++ tree/tree_test.go
@@ -13,25 +13,25 @@ func TestTreeStruct(t *testing.T) {
 		"│── tempfolder3\n3 directories"
 	treestruct := TreeStruct{true, false, false, false, false, false, 0}
 	got := getTree(treestruct, "/Users/chinmaysomani/Desktop/gocodes/tree")
-	assert.Equal(t, want, got, "Not equal")
+	assert.Equal(t, want, got, "Directories only Test")
 
 	want = "/Users/chinmaysomani/Desktop/gocodes/tree\n│── tempfolder\n    │── tempfolder2\n2 directories"
 	treestruct = TreeStruct{true, false, false, false, false, false, 2}
 	got = getTree(treestruct, "/Users/chinmaysomani/Desktop/gocodes/tree")
-	assert.Equal(t, want, got, "Not equal")
+	assert.Equal(t, want, got, "Directories only with Levels test")
 
 	want = "/Users/chinmaysomani/Desktop/gocodes/tree\n│── [-rw-r--r--] go.mod\n│── [-rw-r--r--] go.sum\n│── [drwxr-xr-x] tempfolder\n    " +
 		"│── [drwxr-xr-x] tempfolder2\n│── [-rw-r--r--] tree.go\n│── [-rw-r--r--] tree_test.go\n\n2 directories, 5 files\n"
 	treestruct = TreeStruct{false, false, true, false, false, false, 2}
 	got = getTree(treestruct, "/Users/chinmaysomani/Desktop/gocodes/tree")
-	assert.Equal(t, want, got, "Not equal")
+	assert.Equal(t, want, got, "Permissions with levels test")
 
 	want = "/Users/chinmaysomani/Desktop/gocodes/tree\n│── /Users/chinmaysomani/Desktop/gocodes/tree/tempfolder\n    " +
 		"│── /Users/chinmaysomani/Desktop/gocodes/tree/tempfolder2\n        " +
 		"│── /Users/chinmaysomani/Desktop/gocodes/tree/tempfolder3\n3 directories"
 	treestruct = TreeStruct{true, false, false, true, false, false, 0}
 	got = getTree(treestruct, "/Users/chinmaysomani/Desktop/gocodes/tree")
-	assert.Equal(t, want, got, "Not equal")
+	assert.Equal(t, want, got, "Directories only with relative path test")
 
 	want = "[\n  {\"type\":\"directory\",\"name\":/Users/chinmaysomani/Desktop/gocodes/tree,\"contents:\"[\n     " +
 		"{\"type\":\"file\",\"name\":\"go.mod\"}\n     {\"type\":\"file\",\"name\":\"go.sum\"}\n     " +
@@ -42,10 +42,10 @@ func TestTreeStruct(t *testing.T) {
 		"{\"type\":\"file\",\"name\":\"tree_test.go\"}\n,\n   {\"type\":\"report\",\"directories\":3,\"files\":6}\n]"
 	treestruct = TreeStruct{false, false, false, false, true, false, 0}
 	got = getTree(treestruct, "/Users/chinmaysomani/Desktop/gocodes/tree")
-	assert.Equal(t, want, got, "Not equal")
+	assert.Equal(t, want, got, "Json format test")
 
 	want = "/Users/chinmaysomani/Desktop/gocodes/tree\n│── [drwxr-xr-x] /Users/chinmaysomani/Desktop/gocodes/tree/tempfolder\n2 directories"
 	treestruct = TreeStruct{true, false, true, true, false, false, 1}
 	got = getTree(treestruct, "/Users/chinmaysomani/Desktop/gocodes/tree")
-	assert.Equal(t, want, got, "Not equal")
+	assert.Equal(t, want, got, "Directory only with relative path and permissions at level 1 test")
 }
